931e4b8f5d58261c4409593852d2e39f3b47f160
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2023-02-16 12:38:13 +0530

    Add files via upload

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,150 +1,75 @@
-/*
-1: Create Table in SQL
-2: Insert Table in SQL
-3: Select Statement in SQL (*, Top, Distinct, Count, As, max,min,avg)
-4: Group by, Order by
-*/
+--Insert into CovidDeaths values 
+--('AndamannNicobar',	10747,	0,	10618,	129,	0,	98.8,	1.2,	100896618),
+--('AndhraPradesh',	2339078,	7,	2324338,	14733,	0,	99.37,	0.63,	128500364),
+--('ArunachalPradesh',	66891,	0,	66595,	296,	0,	99.56,	0.44,	658019),
+--('Assam',	746100,	0,	738065,	8035,	0,	98.92,	1.08,	290492),
+--('Bihar',	851404,	1,	839100,	12303,	0,	98.55,	1.45,	40100376),
+--('Chandigarh',	99358,	3,	98174,	1181,	0,	98.81,	1.19,	30501026),
+--('Chhattisgarh',	1177768,	8,	1163614,	14146,	0,	98.8,	1.2,	28900667),
+--('DadranNagarHavelinDamannDiu',	11591,	0,	11587,	4,	0,	99.97,	0.03,	231502578),
+--('Delhi',	2007313,	10,	1980781,	26522,	0,	98.68,	1.32,	773997),
+--('Goa',	259110,	15,	255082,	4013,	0.01,	98.45,	1.55,	3772103),
+--('Gujarat',	1277615,	11,	1266561,	11043,	0,	99.13,	0.86,	70400153),
+--('Haryana',	1056655,	38,	1045903,	10714,	0,	98.98,	1.01,	7503010),
+--('HimachalPradesh',	312692,	14,	308465,	4213,	0,	98.65,	1.35,	3436948),
+--('JammunKashmir',	479444,	10,	474649,	4785,	0,	99,	1,	66001),
+--('Jharkhand',	442574,	0,	437243,	5331,	0,	98.8,	1.2,	124904071),
+--('Karnataka',	4072536,	123,	4032105,	40308,	0,	99.01,	0.99,	1711947),
+--('Kerala',	6829249,	1300,	6756379,	71570,	0.02,	98.93,	1.05,	91702478),
+--('Ladakh',	29417,	1,	29185,	231,	0,	99.21,	0.79,	4184959),
+--('Lakshadweep',	11415,	0,	11363,	52,	0,	99.54,	0.46,	11700099),
+--('MadhyaPradesh',	1054934,	2,	1044155,	10777,	0,	98.98,	1.02,	14999397),
+--('Maharashtra',	8136945,	134,	7988392,	148419,	0,	98.17,	1.82,	399001),
+--('Manipur',	139924,	0,	137775,	2149,	0,	98.46,	1.54,	47099270),
+--('Meghalaya',	96786,	1,	95161,	1624,	0,	98.32,	1.68,	79502477),
+--('Mizoram',	238964,	0,	238238,	726,	0,	99.7,	0.3,	1308967),
+--('Nagaland',	35986,	0,	35204,	782,	0,	97.83,	2.17,	38157311),
+--('Odisha',	1336595,	84,	1327306,	9205,	0.01,	99.31,	0.69,	19301096),
+--('Puducherry',	175636,	73,	173588,	1975,	0.04,	98.83,	1.12,	2073074),
+--('Punjab',	784282,	29,	764964,	19289,	0,	97.54,	2.46,	34698876),
+--('Rajasthan',	1315564,	5,	1305906,	9653,	0,	99.27,	0.73,	1521992),
+--('Sikkim',	44321,	2,	43820,	499,	0,	98.87,	1.13,	83697770),
+--('TamilNadu',	3594573,	58,	3556466,	38049,	0,	98.94,	1.06,	35998752),
+--('Telengana',	841453,	27,	837315,	4111,	0,	99.51,	0.49,	69599762),
+--('Tripura',	108034,	0,	107094,	940,	0,	99.13,	0.87,	1646050),
+--('UttarPradesh',	2128154,	18,	2104502,	23634,	0,	98.89,	1.11,	1158040),
+--('Uttarakhand',	449429,	11,	441665,	7753,	0,	98.27,	1.73,	85002417),
+--('WestBengal',	2118696,	50,	2097114,	21532,	0,	98.98,	1.02,	32199722)
 
---Create table EmployeeDemographics
---(
---EmployeeId int,
---FirstName varchar(50),
---LastName varchar(50),
---Age int,
---Gender varchar(50)
---)
-
---create table WareHouseEmployeeDemographics
---(
---EmployeeId int,
---FirstName varchar(50),
---LastName varchar(50),
---Age int,
---Gender varchar(50)
---)
-
---Create table EmployeePaygrade
---(
---EmployeeId int,
---JobTitle varchar(50),
---Salary int,
---)
-
-
-
---Insert into EmployeeDemographics values
---(1001,'Jim','Halpert',30,'Male'),
---(1002,'Pam','Beasley',30,'Female'),
---(1003,'Dwight','Shcrute',29,'Male'),
---(1004,'Angela','Martin',31,'Female'),
---(1005,'Toby','Flenderson',32,'Male'),
---(1006,'Micheal','Scott',35,'Male'),
---(1007,'Meredith','Palmer',32,'Female'),
---(1008,'Stanley','Hudson',38, 'Male'),
---(1009,'Kevin','Malone',31,'Male')
-
---Insert into EmployeeDemographics values
---(1011, 'Ryan', 'Howard', 26, 'Male'),
---(NULL,'Holly','Flax', NULL, NULL),
---(1013,'Darryl','Philbin', NULL, 'Male')
-
---Insert into EmployeePaygrade values
---(1001,'Salesman',45000),
---(1002,'Receptionist',36000),
---(1003,'Salesman',63000),
---(1004,'Accountant',47000),
---(1005,'HR',50000),
---(1006,'Regional Manager', 65000),
---(1007,'Supplier Relations', 41000),
---(1008,'Salesman',48000),
---(1009,'Accountant',42000)
---insert into EmployeePaygrade values
---(1010, NULL, 47000),
---(NULL,'Salesman', 43000)
-
---insert into WareHouseEmployeeDemographics values
---(1013,'Darryl','Philbin', NULL, 'Male'),
---(1050,'Roy','Anderson',31,'Male'),
---(1051,'Hidetoshi','Hasagawa', 40, 'Male'),
---(1052,'Val','Johnson', 31, 'Female')
-
---Select *
---From EmployeePaygrade
-
---select *
---from EmployeeDemographics
-
---Select top 5 *
---from EmployeeDemographics
-
---select top 5 *
---from EmployeePaygrade
-
---Select Distinct(EmployeeID)
---From EmployeeDemographics
-
---Select Distinct(EmployeeID)
---From EmployeePaygrade
-
---select distinct(Gender)
---from EmployeeDemographics
-
---select distinct(Salary)
---from EmployeePaygrade
-
---select count(FirstName) as FirstNameCount
---From EmployeeDemographics
-
---select count(Salary) as SalaryCount
---from EmployeePaygrade
-
---select max(Age) 
---from EmployeeDemographics
-
---select max(Salary)
---from EmployeePaygrade
-
---select min(Age)
---from EmployeeDemographic
-
---select min(Salary)
---from EmployeePaygrade
-
---select avg(Age)
---from EmployeeDemographics
-
---select avg(Salary)
---from EmployeePaygrade
-
---select *
---from EmployeeDemographics
-
---select Gender, Age, count(Gender)
---from EmployeeDemographics
---Group by Gender, Age
-
---select Gender, count(Gender)
---from EmployeeDemographics
---where age > 31
---group by Gender
-
---select Gender, count(Gender) as CountGender
---from EmployeeDemographics
---where Age > 31
---group by Gender
---order by CountGender desc
-
---select *
---from EmployeeDemographics
---order by Age 
-
---select *
---from EmployeeDemographics
---order by Age desc, Gender desc
-
-/* same as above */
-
---select *
---from EmployeeDemographics
---order by 4 desc, 5 desc
-
+Insert into CovidVaccine Values
+('AndamannNicobar',	629054,	311893,	317161,	399001),
+('AndhraPradesh',	84147957,	40624263,	43523694,	91702478),
+('ArunchalPradesh',	1596166,	856732,	739434,	1711947),
+('Assam',	42998698,	22535419,	20463279,	35998752),
+('Bihar',	119963226,	62590002,	57373224,	128500364),
+('Chandigarh',	2001114,	1088086,	913028,	1158040),
+('Chhattisgarh',	36927545,	18855121,	18072424,	32199722),
+('DadranNagarHavelinDamannDiu',	1319914,	729023,	590891,	773997),
+('Delhi',	30710281,	16555043,	14155238,	19301096),
+('Goa',	2572559,	1353009,	1219550,	1521992),
+('Gujarat',	98534412,	49269034,	49265378,	70400153),
+('Haryana',	40581317,	21938012,	18643305,	28900667),
+('HimachalPradesh',	11770370,	6028184,	5742186,	7503010),
+('JammunKashmir',	20272520,	9936338,	10336182,	14999397),
+('Jharkhand',	36992685,	21243308,	15749377,	40100376),
+('Karnataka',	100209012,	49971474,	50237538,	69599762),
+('Kerala',	50729256,	26999013,	23730243,	34698876),
+('Ladakh',	404656,	219238,	185418,	290492),
+('Lakshadweep',	112378,	56831,	55547,	66001),
+('MadhyaPradesh',	107931053,	54060775,	53870278,	85002417),
+('Maharashtra',	155773246,	84570317,	71202929,	124904071),
+('Manipur',	2666749,	1457120,	1209629,	3436948),
+('Meghalaya',	2348527,	1329229,	1019298,	3772103),
+('Mizoram',	1440084,	783477,	656607,	1308967),
+('Nagaland',	1515042,	835771,	679271,	2073074),
+('Odisha',	60803739,	31331147,	29472592,	47099270),
+('Puducherry',	1620765,	903986,	716779,	1646050),
+('Punjab',	41717794,	22309609,	19408185,	30501026),
+('Rajasthan',	97164120,	51029686,	46134434,	79502477),
+('Sikkim',	1045753,	539208,	506545,	658019),
+('TamilNadu',	107856629,	56110543,	51746086,	83697770),
+('Telangana',	58332610,	29547155,	28785455,	38157311),
+('Tripura',	4962881,	2653391,	2309490,	4184959),
+('UttarPradesh',	299773777,	153669397,	146104380,	231502578),
+('Uttarakhand',	16068172,	8164652,	7903520,	11700099),
+('WestBengal',	128418265,	67232447,	61185818,	100896618)
--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,5 +1,13 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [EmployeeId]
-      ,[JobTitle]
-      ,[Salary]
-  FROM [SQLTutorial].[dbo].[EmployeePaygrade]
+select *
+from CovidDeaths
+
+select *
+from CovidVaccine
+
+-- Select Data that we ae going to use
+select StatesnUTs, TotalCases, Active, Deaths, TotalPopulation
+from CovidDeaths
+order by 1,2
+
+-- Looking at total cases Vs total deaths
+-- Looking at total cases Vs Population
